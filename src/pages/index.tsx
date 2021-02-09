4a192dc947a9167e8b64e0a6071bc2c82aff40a3
Amit Sen <amit@atlasai.us>
2021-02-08 20:28:42 -0800

    Add the jumbotron, description, and portfolio to the home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import Description from "../components/home/description";
+import Jumbotron from "../components/home/jumbotron";
+import Portfolio from "../components/home/portfolio";
 import Navbar from "../components/shared/navbar";
 
 const HomePage = () => {
   return (
     <>
       <Navbar />
-      <div>Welcome</div>
+      <Jumbotron />
+      <Description />
+      <Portfolio />
     </>
   );
 };
