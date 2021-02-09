0147a1304a67c7a6bf52bb360199e955b56b62d9
Amit Sen <amit@atlasai.us>
2021-02-08 22:20:45 -0800

    Create copy component.

---
+++ src/copy/copy.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const portfolioItemsCopy = [
+  {
+    title: "National Scale Logistics",
+    description:
+      "Zipline partners with our customers to serve each of their delivery points across an entire region or country, making products available quickly while minimizing waste. Our system can either complement or replace existing delivery networks.",
+    caption: "Map of Zipline service area in Rwanda",
+  },
+  {
+    title: "Healthcare Expertise",
+    description:
+      "Zipline understands that quality storage and handling are paramount, particularly for medical products that require cold chain and other special conditions. Our healthcare expertise informs end-to-end product storage and handling that meet the industry’s high standards.",
+    caption: null,
+  },
+  {
+    title: "Customer Centered, Tech Enabled",
+    description:
+      "Zipline designs, tests, manufactures, and operates our cutting-edge technology. Our drones and autonomous navigation systems have been custom developed to meet your delivery needs, and we never stop improving them.",
+    caption:
+      "Vice President Mahamudu Bawumia launches Zipline’s service in Ghana, April 2019",
+  },
+  {
+    title: "Light, Fast, Electric",
+    description:
+      "Gas combustion vehicles break down, get stuck in traffic jams that prevent urgent response, and put human drivers at risk behind the wheel, particularly when the route is rough and treacherous. Zipline’s drones are battery powered and fly quickly and directly to their destinations, leaving ground vehicles behind.",
+    caption: null,
+  },
+  {
+    title: (
+      <>
+        High Tech Jobs
+        <br /> for Local Talent
+      </>
+    ),
+    description:
+      "Zipline hires and trains local talent to oversee our flight and fulfillment operations in every market we operate. Every Zipline installation doubles as an investment in local job development and training.",
+    caption: null,
+  },
+];
+
+export { portfolioItemsCopy };
