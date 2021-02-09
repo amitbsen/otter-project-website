ad9ff58d2176e8fd036fe922d35d86dea78469e7
Amit Sen <amit@atlasai.us>
2021-02-08 20:28:29 -0800

    Center the container by default.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      center: true,
+    },
     extend: {},
   },
   variants: {
