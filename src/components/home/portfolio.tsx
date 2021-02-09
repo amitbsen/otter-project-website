0087babf16ddb0f47b4c22fa05956f52dfadeb0f
Amit Sen <amit@atlasai.us>
2021-02-08 20:28:05 -0800

    Implement home/portfolio.

---
+++ src/components/home/portfolio.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Portfolio = () => {
+  return (
+    <div className="container max-w-screen-lg py-20 px-10 font-sans">
+      <h1 className="font-bold mb-0 max-w-2xl text-6xl leading-snug">
+        How We're Different
+      </h1>
+      <h2 className="font-normal text-gray-400 mt-0 max-w-4xl leading-normal text-4xl mb-14">
+        Our mission is to provide every human on Earth with instant access to
+        vital medical supplies.
+      </h2>
+
+      <p className="text-2xl leading-normal mb-5">
+        Recent advances in logistics — like instant, on-demand delivery — mainly
+        serve those in the biggest, wealthiest cities.
+      </p>
+      <p className="text-2xl leading-normal mb-5">
+        Zipline is a transformational change in logistics. We provide equal
+        access to on-demand delivery, globally, and for the deliveries that
+        matter most.
+      </p>
+    </div>
+  );
+};
+
+export default Portfolio;
