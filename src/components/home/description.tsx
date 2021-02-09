633dea806c9d1aaafc5e67b702941603925f1a47
Amit Sen <amit@atlasai.us>
2021-02-08 20:27:50 -0800

    Implement home/description.

---
+++ src/components/home/description.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Description = () => {
+  return (
+    <div className="bg-black">
+      <div className="container max-w-screen-lg py-16 px-10">
+        <h2 className="text-white leading-normal font-normal">
+          Zipline delivers critical and lifesaving products precisely where and
+          when they are needed, safely and reliably, every day, across multiple
+          countries.
+        </h2>
+        <h2 className="text-white leading-normal font-normal">
+          We've made <b>101,355</b> commercial deliveries — and counting.
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Description;
